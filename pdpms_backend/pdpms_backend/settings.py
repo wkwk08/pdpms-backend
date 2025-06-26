15b8af11bf869b937a621c012bf2f88ed96d41c3
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-26 16:04:41 +0800

    Add documents to installed apps

--- pdpms_backend/pdpms_backend/settings.py
+++ pdpms_backend/pdpms_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'activity_logs',
+    'documents',
     'rest_framework',
     'corsheaders',
 # Optional: enable if using AWS Cognito for JWT-based authentication
