c69b2f680937b28f0822591eafdeda8a9c2c6f46
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-26 15:53:03 +0800

    Add activity_logs to installed apps

--- pdpms_backend/pdpms_backend/settings.py
+++ pdpms_backend/pdpms_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'activity_logs',
     'rest_framework',
     'corsheaders',
 # Optional: enable if using AWS Cognito for JWT-based authentication
