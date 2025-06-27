060bcbe79c27347f29b887143cb73a6225e86390
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-27 09:37:51 +0800

    Include documents URLs in project routing

--- pdpms_backend/pdpms_backend/urls.py
+++ pdpms_backend/pdpms_backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('activity_logs.urls')),
+    path('api/', include('documents.urls')),
 ]
