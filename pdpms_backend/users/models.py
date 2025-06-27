92c164aa1cb7466fda055773679c56009ade9c0e
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-27 11:11:27 +0800

    Update users models, serializers, and views

--- pdpms_backend/users/models.py
+++ pdpms_backend/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255, primary_key=True)
+    employee_id = models.CharField(max_length=255)
+    user_password = models.CharField(max_length=255)
+    access_level = models.CharField(max_length=255)
