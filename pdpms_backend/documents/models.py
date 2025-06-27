4506a69044c153e1e8f78c025a01f6fbdf504cee
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-27 09:37:32 +0800

    Update documents models, serializers, views, and URLs

--- pdpms_backend/documents/models.py
+++ pdpms_backend/documents/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    document_id = models.CharField(max_length=255, primary_key=True)
+    reference_code = models.CharField(max_length=255)
+    subject = models.TextField()
+    document_type = models.CharField(max_length=255)
+    document_date = models.DateField()
+    date_received = models.DateField()
+    assigned_to = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    remarks = models.TextField()
+
+    class Meta:
+        db_table = 'documents'
+        managed = False
+
+    def __str__(self):
+        return f"{self.document_id} - {self.subject}"
--- pdpms_backend/documents/views.py
+++ pdpms_backend/documents/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Document
+from .serializers import DocumentSerializer
 
-# Create your views here.
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
