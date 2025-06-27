621352fb8c266114fe34e899b6c5631d5399523d
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-27 10:36:39 +0800

    Update properties models, serializers, and views

--- pdpms_backend/properties/views.py
+++ pdpms_backend/properties/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Property
+from .serializers import PropertySerializer
 
-# Create your views here.
+class PropertyViewSet(viewsets.ModelViewSet):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
