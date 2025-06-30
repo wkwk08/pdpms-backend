eef4c2a87ae67f79c8b536830a8949c229419aa9
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-30 10:55:00 +0800

    Add UnserviceableProperty to properties app

--- pdpms_backend/properties/serializers.py
+++ pdpms_backend/properties/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Property
 from .models import ServiceableProperty
+from .models import UnserviceableProperty
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +11,9 @@ class PropertySerializer(serializers.ModelSerializer):
 class ServiceablePropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceableProperty
+        fields = '__all__'
+
+class UnserviceablePropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UnserviceableProperty
         fields = '__all__'
--- pdpms_backend/properties/views.py
+++ pdpms_backend/properties/views.py
@@ -4,6 +4,8 @@ from .models import Property
 from .serializers import PropertySerializer
 from .models import ServiceableProperty
 from .serializers import ServiceablePropertySerializer
+from .models import UnserviceableProperty
+from .serializers import UnserviceablePropertySerializer
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
@@ -12,3 +14,7 @@ class PropertyViewSet(viewsets.ModelViewSet):
 class ServiceablePropertyViewSet(viewsets.ModelViewSet):
     queryset = ServiceableProperty.objects.all()
     serializer_class = ServiceablePropertySerializer
+
+class UnserviceablePropertyViewSet(viewsets.ModelViewSet):
+    queryset = UnserviceableProperty.objects.all()
+    serializer_class = UnserviceablePropertySerializer
