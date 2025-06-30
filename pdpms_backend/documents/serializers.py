43a3822eff8cb090c244d113e2a86cc7560317d2
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-30 10:00:22 +0800

    Add CompletedDocument model to documents app

--- pdpms_backend/documents/serializers.py
+++ pdpms_backend/documents/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Document
+from .models import CompletedDocument
 
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
+        fields = '__all__'
+
+class CompletedDocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompletedDocument
         fields = '__all__'
--- pdpms_backend/documents/views.py
+++ pdpms_backend/documents/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Document
 from .serializers import DocumentSerializer
+from .models import CompletedDocument
+from .serializers import CompletedDocumentSerializer
 
 class DocumentViewSet(viewsets.ModelViewSet):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
 
+class CompletedDocumentViewSet(viewsets.ModelViewSet):
+    queryset = CompletedDocument.objects.all()
+    serializer_class = CompletedDocumentSerializer
