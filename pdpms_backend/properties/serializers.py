f2714b38186cda3e88e4151dd2a82874dbc745fe
wkwk08 <jerichoambrocio123@gmail.com>
2025-06-30 10:39:19 +0800

    Add ServiceableProperty model to properties app

--- pdpms_backend/properties/serializers.py
+++ pdpms_backend/properties/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Property
+from .models import ServiceableProperty
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
+        fields = '__all__'
+
+class ServiceablePropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceableProperty
         fields = '__all__'
--- pdpms_backend/properties/views.py
+++ pdpms_backend/properties/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Property
 from .serializers import PropertySerializer
+from .models import ServiceableProperty
+from .serializers import ServiceablePropertySerializer
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+
+class ServiceablePropertyViewSet(viewsets.ModelViewSet):
+    queryset = ServiceableProperty.objects.all()
+    serializer_class = ServiceablePropertySerializer
