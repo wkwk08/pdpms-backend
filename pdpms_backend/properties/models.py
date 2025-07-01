012780ead38c1e8387faf8e0a0223e03d7d30a61
wkwk08 <jerichoambrocio123@gmail.com>
2025-07-01 08:58:09 +0800

    Rename status to property_status in UnserviceableProperty

--- pdpms_backend/properties/models.py
+++ pdpms_backend/properties/models.py
@@ -50,7 +50,7 @@ class UnserviceableProperty(models.Model):
     unit_cost = models.DecimalField(max_digits=10, decimal_places=2)
     end_user = models.CharField(max_length=255)
     estimated_life_use = models.IntegerField()
-    status = models.CharField(max_length=255)
+    property_status = models.CharField(max_length=255)
     remarks = models.TextField()
 
     class Meta:
