50ba1ce0e44743e4de8bc6ea3ec53adfb56e9e80
wkwk08 <jerichoambrocio123@gmail.com>
2025-07-01 08:52:42 +0800

    Rename status to document_status in OnGoingDocument

--- pdpms_backend/documents/models.py
+++ pdpms_backend/documents/models.py
@@ -44,7 +44,7 @@ class OnGoingDocument(models.Model):
     document_date = models.DateField()
     date_received = models.DateField()
     received_by = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)
+    document_status = models.CharField(max_length=255)
     remarks = models.TextField()
 
     class Meta:
