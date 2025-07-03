0b932362b9a35526693ca1825175da71a308e738
wkwk08 <jerichoambrocio123@gmail.com>
2025-07-03 23:44:46 +0800

    feat(UserViewSet): validate current password before allowing password updates

--- pdpms_backend/users/views.py
+++ pdpms_backend/users/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        current_password = request.data.get('current_password')
+        new_password = request.data.get('user_password')
+
+        # Only check password if user_password is being changed
+        if new_password:
+            if not current_password or current_password != instance.user_password:
+                return Response({'detail': 'Current password is incorrect.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return super().update(request, *args, **kwargs)
